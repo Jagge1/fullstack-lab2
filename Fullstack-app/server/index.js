bafda2ee62d8591559470a327e00cb0e9e5199f1
Jacob <hellgrenjacob@hotmail.com>
2025-04-21 11:34:33 +0200

    logic to add new project to database

--- Fullstack-app/server/index.js
+++ Fullstack-app/server/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import Employee from './models/employee.model.js'
-import ProjectAssignment from './models//projectAssignment.model.js'
+import ProjectAssignment from './models/projectAssignment.model.js'
 import Project from './models//project.model.js'
 import bcrypt from 'bcrypt';
 dotenv.config();
@@ -23,24 +23,25 @@ app.post('/api/employees', async (req, res)=>{
   //Using bcrypt to hash userpassword
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(hashed_password, salt);
-  const count = await Employee.countDocuments();
-  const newId = 'E'+ (count + 1);
+  //counting documents in collection to use for unique id
+  const eCount = await Employee.countDocuments();
+  const newEId = 'E'+ (eCount + 1);
 
   try {
-    const employees = await Employee.find({employee_id: newId})    
+    const employees = await Employee.find({employee_id: newEId})    
     if (employees.length > 0) {
       return res.status(409).json({message: 'Error: employee already exists!'})
     }
     //New Employee with hashed password
     const newEmployee = {
-      employee_id: newId,
+      employee_id: newEId,
       full_name,
       email,
       hashed_password: hash //The password that was hashed
     }
     //The new employee get created
     await Employee.create(newEmployee);
-    res.status(201).json({message: 'Employee was created'})
+    res.status(201).json({message: 'Employee was created'});
     
   } catch (error) {
     return res.status(500).json({error: error.message});
@@ -48,7 +49,28 @@ app.post('/api/employees', async (req, res)=>{
 });
 
 app.post('/api/projects', async (req, res)=> {
+  const {project_code, project_name, project_description} = req.body
+  const pCount = await Project.countDocuments();
+  const newPCode = 'P'+ (pCount + 1);
 
+  try {
+    const projects = await Project.find({project_code: newPCode}); 
+    if (projects.length > 0) {
+      return res.status(409).json({message: 'Project already exists'})
+    }
+
+    const newProject = {
+      project_code: newPCode,
+      project_name,
+      project_description
+    }
+
+    await Project.create(newProject);
+    res.status(201).json({message: 'Project was created'})
+    
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
 });
 
 app.post('/api/project_assignments', async (req, res)=> {
